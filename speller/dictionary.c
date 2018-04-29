3d15c787f7464326596eedc3f201292266bcf7ce
Josh Bristol <josh.bristol2@gmail.com>
2018-04-29 05:01:35 +0000

    added comments

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -8,24 +8,25 @@
 #include <strings.h>
 #include "dictionary.h"
 
-// #define HASHSIZE 26
+#define HASHSIZE 26
 
 typedef struct node
 {
-    char word[LENGTH + 1];  //stores the word in the node
+    char word[LENGTH + 1];  //stores the word in the node , value
     struct node *next;  // pointer for the next item in the list
 }
-node;
+node;  // names the node
 
-node *hashtable[26];
+node *hashtable[HASHSIZE];
 
-int countWord;
-int hashIndex;
+int countWord;  // instantiate a variable to increment when we found a word
+int hashIndex;  // for tracking location in the list
 
 int hashCode(const char *key)
 {
-    int hash = (tolower(key[0]) - 'A');
-    return hash % 26;
+    char firstChar = key[0];
+    int hash = tolower(firstChar) - 'a';  // sets the index to ascii lowercase
+    return hash % HASHSIZE;  // returns the index between 0 & 26
 }
 
 
@@ -34,11 +35,11 @@ int hashCode(const char *key)
 bool load(const char *dictionary)
 {
     char word[LENGTH + 1];
-    FILE *file = fopen(dictionary, "r");  // opens dictionary
+    FILE *file = fopen(dictionary, "r");  // opens dictionary in read only permissions
 
     if (file == NULL)  // checks to make sure that it loads
     {
-        unload();
+        unload();  // if failed, dump the memory
         return false;
     }
 
@@ -55,34 +56,34 @@ bool load(const char *dictionary)
             strcpy(new_node->word, word);  // if malloc is successful, copy word into the node
             new_node->next = NULL;  // sets the "next" for a new_node to NULL
 
-            hashIndex = hashCode(word);
+            hashIndex = hashCode(word);  // sets hashIndex
             // hashIndex = hashCode[word];
-            new_node->next = hashtable[hashIndex];  // set the head
+            new_node->next = hashtable[hashIndex];  // set the next pointer
             hashtable[hashIndex] = new_node;
 
-            countWord++;
+            countWord++;  // increments variable to count words
         }
     }
 
-    fclose(file);
+    fclose(file);  // once complete, close dictionary
     return true;
 }
 
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    hashIndex = hashCode(word);
-    node *cursor = hashtable[hashIndex];
+    hashIndex = hashCode(word);  // sets the index to the current word
+    node *cursor = hashtable[hashIndex];  // sets the pointer to the cursor of the bucket
 
-    while (cursor != NULL)  // checks to see if the word is in the hashtable
+    while (cursor != NULL)  // checks to see if the cursor is valid
     {
-        if ((strcasecmp(word, cursor->word)) == 0)
+        if ((strcasecmp(word, cursor->word)) == 0)  //  checks for a match, case insensitive
         {
             return true;
         }
         else
         {
-            cursor = cursor->next;
+            cursor = cursor->next; // otherwise advance the cursor
         }
     }
     return false;
@@ -91,9 +92,9 @@ bool check(const char *word)
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    if (countWord > 0)
+    if (countWord > 0)  // checks to see if word count is greater then 0
     {
-        return countWord;
+        return countWord;  // returns quantity of words.
     }
     else
     {
@@ -104,15 +105,15 @@ unsigned int size(void)
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    for (int i = 0; i < 26; i++)
+    for (int i = 0; i < HASHSIZE; i++)  // iterating through the
     {
-        node *cursor = hashtable[i];
-        while ( cursor != NULL)
+        node *cursor = hashtable[i];  // assigning a new node cursor to the head
+        while ( cursor != NULL)  // itterates as long as the cursor is not NULL
         {
-            node *temp = cursor;
-            cursor = cursor->next;
-            free(temp);
+            node *temp = cursor;  // create a temp node and assign it to cursor
+            cursor = cursor->next;  // advance the cursor
+            free(temp);  // free the cursor
         }
     }
-    return true;
+    return true;  // if successful return true
 }
